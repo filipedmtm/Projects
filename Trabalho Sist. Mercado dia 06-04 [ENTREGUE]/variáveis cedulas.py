76eb4ef811603cf8bc3b27d1b1dccfdebebcd2a1
filipe mota <129749910+filipedmtm@users.noreply.github.com>
2023-06-10 00:56:20 -0300

    Create variáveis cedulas.py

---
+++ Trabalho Sist. Mercado dia 06-04 [ENTREGUE]/variáveis cedulas.py
@@ -0,0 +1,10 @@
+ced200=200.00
+ced100=100.00
+ced50=50.00
+ced10=10.00
+ced5=5.00
+mod1=1.00
+mod50=0.50
+valor_abertura=2*ced200+4*ced100+6*ced50+10*ced10+10*ced5+20*mod1+20*mod50
+
+print(valor_abertura)
